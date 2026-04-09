ca4863c28cfc9a7c1582876c01d15334cbaa60a6
sand01man <imhotep.holloway@outlook.com>
2026-04-09 17:11:07 -0400

    Lesson 7 final commit

--- mob_spawner.gd
+++ mob_spawner.gd
@@ -1,38 +1,38 @@
 class_name MobSpawner extends Node2D
-#@onready var _path: Path2D = %Path2D
-#var mob_packed_scene = preload("res://mobs/mob.tscn")
-#@onready var timer: Timer = %Timer
-#@export var timer_wait_time = 2
-#@export var total_mobs : int  = 100
-#@export var wave_amount : int = 10
-#@export var wave_interval := 30
-#var mobs_this_wave := 2
-#var waves_begun = 0
-#func _ready() -> void:
-	#timer.timeout.connect(spawn_mob)
-	#timer.timeout.connect(mob_amount_counter)
-	#timer.wait_time = wave_interval
-	#spawn_mob()
-	#waves_begun += 1
-	#timer.start()
-	#
-	#
-#func spawn_mob() -> void:
-	#if waves_begun <= wave_amount + (waves_begun * 2):
-		#for i in mobs_this_wave:
-			 #
-			#
-			#await get_tree().create_timer(timer_wait_time).timeout
-		#
-			#var mob_path_follow := MobPathFollow.new()
-			#_path.add_child(mob_path_follow)
-		#
-		#
-			#var mob : Mob = mob_packed_scene.instantiate()
-			#mob_path_follow.add_child(mob)
-			#mob_path_follow.mob = mob
-#func mob_amount_counter() -> void:
-	#waves_begun += 1
-	#
-	#
-## Called when the node enters the scene tree for the first time.
+@onready var _path: Path2D = %Path2D
+var mob_packed_scene = preload("res://mobs/mob.tscn")
+@onready var timer: Timer = %Timer
+@export var timer_wait_time = 2
+@export var total_mobs : int  = 100
+@export var wave_amount : int = 10
+@export var wave_interval := 30
+var mobs_this_wave := 2
+var waves_begun = 0
+func _ready() -> void:
+	timer.timeout.connect(spawn_mob)
+	timer.timeout.connect(mob_amount_counter)
+	timer.wait_time = wave_interval
+	spawn_mob()
+	waves_begun += 1
+	timer.start()
+	
+	
+func spawn_mob() -> void:
+	if waves_begun <= wave_amount:
+		for i in mobs_this_wave + (waves_begun * 2):
+			 
+			
+			await get_tree().create_timer(timer_wait_time).timeout
+		
+			var mob_path_follow := MobPathFollow.new()
+			_path.add_child(mob_path_follow)
+		
+		
+			var mob : Mob = mob_packed_scene.instantiate()
+			mob_path_follow.add_child(mob)
+			mob_path_follow.mob = mob
+func mob_amount_counter() -> void:
+	waves_begun += 1
+	
+	
+# Called when the node enters the scene tree for the first time.
